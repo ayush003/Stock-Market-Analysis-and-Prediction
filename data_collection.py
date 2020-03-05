e29e49ac2e6f13faf1e7a238e73cb72e93e0091a
Ayush <ayushme1998@gmail.com>
2020-03-05 23:02:44 +0530

    Candlestick Started

--- data_collection.py
+++ data_collection.py
@@ -3,15 +3,16 @@ from bs4 import BeautifulSoup  # lib for pulling data from html/xmlsites
 from urllib.request import urlopen
 
 for i in range(1,2): # lib for sending and rec info over http
-    Url = 'http://www.moneycontrol.com/stocks/hist_stock_result.php?sc_id=KMF&pno='+str(i)+'&hdn=daily&fdt=2010-01-01&todt=2020-02-01'
+    Url = 'http://www.moneycontrol.com/stocks/hist_stock_result.php?sc_id=UTI10&pno='+str(i)+'2&hdn=daily&fdt=2015-01-01&todt=2019-01-01'
     pageHtml = urlopen(Url)
     soup = BeautifulSoup(pageHtml,"html.parser")
     table = soup.find_all("table", { "class" : "tblchart" })
-    with open('kotak.csv', 'a',newline='') as csvfile:
+    with open('hdfc.csv', 'a',newline='') as csvfile:
         f = csv.writer(csvfile)
         f.writerow(['Date','Open','High','Low','Close','Volume','Spread'])  # headers
         for x in table:
             rows = x.find_all('tr') #find all tr tag
+            print(rows)
             for tr in rows:
                 data=[]
                 cols = tr.find_all('td') #find all td tags
