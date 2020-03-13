2367c894b3c0e1ae4197c01d6e6f13d5ab26f8bd
ayush003 <50289547+ayush003@users.noreply.github.com>
2020-03-13 18:42:34 +0530

    Update candle.py

--- candle.py
+++ candle.py
@@ -2,55 +2,178 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('housingFin.csv',index_col=0)
-print(data.shape)
-print(data.head())
+moving_10_large = []
+mom_large = []
 
-data = data.drop(['Adjustment Type','Adjustment Factor','Volume'],axis=1)
-data.head()
-
-window = [5,10,20,50,100,200,400,500]
-
-for win in window:
-    mean_i = data['Close'].rolling(win).mean()
-    std_dev_i = (data['Close'].rolling(win).var())**0.5
+def bul_eglf(lst_0,lst_1,lst_2):    
+    O_0,H_0,L_0,C_0=lst_0[0],lst_0[1],lst_0[2],lst_0[3]
+    O_1,H_1,L_1,C_1=lst_1[0],lst_1[1],lst_1[2],lst_1[3]
+    O_2,H_2,L_2,C_2=lst_2[0],lst_2[1],lst_2[2],lst_2[3]
     
-    # plt.figure(figsize=(20,10))
-    # plt.plot(data.index,data['Close'],color='black')
-    # plt.plot(data.index,mean_i,color='blue')
-    # plt.fill_between(data.index,mean_i+std_dev_i,mean_i-std_dev_i,color='orange',alpha=0.5)
-    # plt.show()
+    return ((C_1 > O_1) & (O_0 > C_0)) & ((O_0 >= C_1) & (O_1 >= C_0)) & ((O_0 - C_0) > (C_1 - O_1 ))
+
+def bear_eglf(lst_0,lst_1,lst_2):    
+    O_0,H_0,L_0,C_0=lst_0[0],lst_0[1],lst_0[2],lst_0[3]
+    O_1,H_1,L_1,C_1=lst_1[0],lst_1[1],lst_1[2],lst_1[3]
+    O_2,H_2,L_2,C_2=lst_2[0],lst_2[1],lst_2[2],lst_2[3]
     
-    profit = 0
-    upper_band = mean_i+std_dev_i
-    lower_band = mean_i-std_dev_i
+    return (O_1 > C_1) & (C_0 > O_0) & (C_0 >= O_1) & (C_1 >= O_0) & ((C_0 - O_0) > (O_1 - C_1 ))
 
-    buy_price = 0
-    sell_price = 0
+def bul_har(lst_0,lst_1,lst_2):    
+    O_0,H_0,L_0,C_0=lst_0[0],lst_0[1],lst_0[2],lst_0[3]
+    O_1,H_1,L_1,C_1=lst_1[0],lst_1[1],lst_1[2],lst_1[3]
+    O_2,H_2,L_2,C_2=lst_2[0],lst_2[1],lst_2[2],lst_2[3]
+    
+    return (O_1 > C_1) & (C_0 > O_0) & (C_0 <= O_1) & (C_1 <= O_0) & ((C_0 - O_0) < (O_1 - C_1))
 
-    buy = True
-    first = True
+def bear_har(lst_0,lst_1,lst_2):    
+    O_0,H_0,L_0,C_0=lst_0[0],lst_0[1],lst_0[2],lst_0[3]
+    O_1,H_1,L_1,C_1=lst_1[0],lst_1[1],lst_1[2],lst_1[3]
+    O_2,H_2,L_2,C_2=lst_2[0],lst_2[1],lst_2[2],lst_2[3]
+    
+    return (C_1 > O_1) & (O_0 > C_0) & (O_0 <= C_1) & (O_1 <= C_0) & ((O_0 - C_0) < (C_1 - O_1 ))
 
-    for index,x in enumerate(data['Close'][20:]):
-        if first and x>upper_band[index-1]:
-            first = False
-            sell_price = x
-            buy = True
-            continue
+def eve_star(lst_0,lst_1,lst_2):    
+    O_0,H_0,L_0,C_0=lst_0[0],lst_0[1],lst_0[2],lst_0[3]
+    O_1,H_1,L_1,C_1=lst_1[0],lst_1[1],lst_1[2],lst_1[3]
+    O_2,H_2,L_2,C_2=lst_2[0],lst_2[1],lst_2[2],lst_2[3]
+    
+    return (C_2 > O_2) & (min(O_1, C_1) > C_2) & (O_0 < min(O_1, C_1)) & (C_0 < O_0 )  
 
-        if first and x<lower_band[index-1]:
-            first = False
-            buy_price = x
-            buy = False
-            continue
+def mor_star(lst_0,lst_1,lst_2):    
+    O_0,H_0,L_0,C_0=lst_0[0],lst_0[1],lst_0[2],lst_0[3]
+    O_1,H_1,L_1,C_1=lst_1[0],lst_1[1],lst_1[2],lst_1[3]
+    O_2,H_2,L_2,C_2=lst_2[0],lst_2[1],lst_2[2],lst_2[3]
+    
+    return (C_2 < O_2) & (min(O_1, C_1) < C_2) & (O_0 > min(O_1, C_1)) & (C_0 > O_0 )
 
-        if not buy and x>upper_band[index-1]:
-            profit += x-buy_price
-            sell_price = x
-            buy = True
-        elif buy and x<lower_band[index-1]:
-            profit += sell_price-x
-            buy_price = x
-            buy = False
-        
-    print(profit,win)
+def bul_eglf_func(arr):
+	bul_eglf_arr = []
+	for c in range(2,len(arr['Close'])):
+	    lst_2=[arr['Open'].iloc[c-2],arr['High'].iloc[c-2],arr['Low'].iloc[c-2],arr['Close'].iloc[c-2]]
+	    lst_1=[arr['Open'].iloc[c-1],arr['High'].iloc[c-1],arr['Low'].iloc[c-1],arr['Close'].iloc[c-1]]
+	    lst_0=[arr['Open'].iloc[c],arr['High'].iloc[c],arr['Low'].iloc[c],arr['Close'].iloc[c]]
+	    if bul_eglf(lst_0,lst_1,lst_2) and moving_10_large[c]<moving_10_large[c-1]:
+	        bul_eglf_arr.append(c)
+
+	for x in bul_eglf_arr[::-1]:
+	  count = 0
+	  for y in range(x,min(x+15,len(moving_10_large))):
+	    if mom_large[y]>0:
+	      count+=1
+	  if count>=5:
+	    return x
+	return -1
+
+def bear_eglf_func(arr):
+	bear_eglf_arr = []
+	for c in range(2,len(arr['Close'])):
+	    lst_2=[arr['Open'].iloc[c-2],arr['High'].iloc[c-2],arr['Low'].iloc[c-2],arr['Close'].iloc[c-2]]
+	    lst_1=[arr['Open'].iloc[c-1],arr['High'].iloc[c-1],arr['Low'].iloc[c-1],arr['Close'].iloc[c-1]]
+	    lst_0=[arr['Open'].iloc[c],arr['High'].iloc[c],arr['Low'].iloc[c],arr['Close'].iloc[c]]
+	    if bear_eglf(lst_0,lst_1,lst_2) and moving_10_large[c]>moving_10_large[c-1]:
+	        bear_eglf_arr.append(c)
+
+	for x in bear_eglf_arr[::-1]:
+	  count = 0
+	  for y in range(x,min(x+20,len(moving_10_large))):
+	    if mom_large[y]<0:
+	      count+=1
+	  if count>=5:
+	    return x
+	return -1
+
+def bul_har_func(arr):
+	bul_har_arr = []
+	for c in range(2,len(arr['Close'])):
+	    lst_2=[arr['Open'].iloc[c-2],arr['High'].iloc[c-2],arr['Low'].iloc[c-2],arr['Close'].iloc[c-2]]
+	    lst_1=[arr['Open'].iloc[c-1],arr['High'].iloc[c-1],arr['Low'].iloc[c-1],arr['Close'].iloc[c-1]]
+	    lst_0=[arr['Open'].iloc[c],arr['High'].iloc[c],arr['Low'].iloc[c],arr['Close'].iloc[c]]
+	    if bul_har(lst_0,lst_1,lst_2) and moving_10_large[c]<moving_10_large[c-1]:
+	        bul_har_arr.append(c)
+	for x in bul_har_arr[::-1]:
+		count = 0
+		for y in range(x,min(x+15,len(moving_10_large))):
+			if mom_large[y]>0:
+				count+=1
+			if count>=5:
+				return x
+	return -1
+
+def bear_har_func(arr):
+	bear_har_arr = []
+	for c in range(2,len(arr['Close'])):
+	    lst_2=[arr['Open'].iloc[c-2],arr['High'].iloc[c-2],arr['Low'].iloc[c-2],arr['Close'].iloc[c-2]]
+	    lst_1=[arr['Open'].iloc[c-1],arr['High'].iloc[c-1],arr['Low'].iloc[c-1],arr['Close'].iloc[c-1]]
+	    lst_0=[arr['Open'].iloc[c],arr['High'].iloc[c],arr['Low'].iloc[c],arr['Close'].iloc[c]]
+	    if bear_har(lst_0,lst_1,lst_2) and moving_10_large[c]>moving_10_large[c-1]:
+	        bear_har_arr.append(c)
+
+
+	for x in bear_har_arr[::-1]:
+		count = 0
+		for y in range(x,min(x+20,len(moving_10_large))):
+			if mom_large[y]<0:
+			  count+=1
+			if count>=5:
+				return x
+	return -1
+
+def eve_star_func(arr):
+	eve_star_arr = []
+	for c in range(2,len(arr['Close'])):
+	    lst_2=[arr['Open'].iloc[c-2],arr['High'].iloc[c-2],arr['Low'].iloc[c-2],arr['Close'].iloc[c-2]]
+	    lst_1=[arr['Open'].iloc[c-1],arr['High'].iloc[c-1],arr['Low'].iloc[c-1],arr['Close'].iloc[c-1]]
+	    lst_0=[arr['Open'].iloc[c],arr['High'].iloc[c],arr['Low'].iloc[c],arr['Close'].iloc[c]]
+	    if eve_star(lst_0,lst_1,lst_2) and moving_10_large[c]>moving_10_large[c-1]:
+	        eve_star_arr.append(c)
+
+	for x in eve_star_arr:
+		count = 0
+		for y in range(x,min(x+20,len(moving_10_large))):
+			if mom_large[y]<0:
+				count+=1
+			if count>=5:
+				return x
+	return -1
+
+def mor_star_func(arr):
+	mor_star_arr = []
+	for c in range(2,len(arr['Close'])):
+	    lst_2=[arr['Open'].iloc[c-2],arr['High'].iloc[c-2],arr['Low'].iloc[c-2],arr['Close'].iloc[c-2]]
+	    lst_1=[arr['Open'].iloc[c-1],arr['High'].iloc[c-1],arr['Low'].iloc[c-1],arr['Close'].iloc[c-1]]
+	    lst_0=[arr['Open'].iloc[c],arr['High'].iloc[c],arr['Low'].iloc[c],arr['Close'].iloc[c]]
+	    if mor_star(lst_0,lst_1,lst_2) and moving_10_large[c]<moving_10_large[c-1]:
+	        mor_star_arr.append(c)
+
+	for x in mor_star_arr:
+		count = 0
+		for y in range(x,min(x+15,len(moving_10_large))):
+			if mom_large[y]>0:
+				count+=1
+			if count>=5:
+				return x
+	return -1
+
+def candle_pred(data):
+	global moving_10_large
+	global mom_large
+	if len(data['Close'])>100: data = data[-100:]
+	moving_10_large = data['Close'].rolling(10).mean()
+	mom_large = np.gradient(moving_10_large)
+
+	# print("Bullish Engulfing found at : ",bul_eglf_func(data))
+	# print("Bearish Engulfing found at : ",bear_eglf_func(data))
+	# print("Bullish Harami found at : ",bul_har_func(data))
+	# print("Bearish Harami found at : ",bear_har_func(data))
+	# print("Evening Star found at : ",eve_star_func(data))
+	# print("Morning Star found at : ",mor_star_func(data))
+
+	result_array = np.array([bul_eglf_func(data),bear_eglf_func(data),bul_har_func(data),bear_har_func(data),eve_star_func(data),mor_star_func(data)])
+	res_index = np.argmax(result_array)
+	
+	if result_array.sum() == -6:
+		return -1
+
+	return (0 if (res_index&1) else 1)
+
